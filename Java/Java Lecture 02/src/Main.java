0b4e4cca6aa5dfcf337257e48af886c8199cb41f
Programmer-Jaf <programmerjaf@gmail.com>
2025-05-27 21:39:32 +0600

    Breakdown the Java program

---
+++ Java/Java Lecture 02/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Java Program Breakdown");
+    // This is single line comment
+    /*
+    public --> public is an access modifier in java
+    class --> class is a user defined blue-print with some properties and behaviours
+    Main --> Every Program in Java start with Main Function/Method
+    static --> static allows to call methods without creating classes
+    void --> void is the return type. void means null
+    (String[] args) --> it's a command line arguments
+    System.out.println --> help us to print what ever we want in console
+    */
+  }
+}
+
